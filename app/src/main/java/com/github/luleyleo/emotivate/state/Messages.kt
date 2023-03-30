c44c25c2e07e5b3c3f4d07054d9af1adf3c89739
Leopold Luley <git@leopoldluley.de>
2023-03-30 11:55:04 +0200

    Update smileys.

--- app/src/main/java/com/github/luleyleo/emotivate/state/Messages.kt
+++ app/src/main/java/com/github/luleyleo/emotivate/state/Messages.kt
@@ -15,12 +15,12 @@ data class Message(
 @Immutable
 data class Emotion(val valence: String, val arousal: String) {
     fun emote() = when {
-        this.valence == "positive" && this.arousal == "low" -> "\uD83D\uDE04"
-        this.valence == "positive" && this.arousal == "mid" -> "\uD83D\uDE04"
-        this.valence == "positive" && this.arousal == "high" -> "\uD83D\uDE04"
-        this.valence == "negative" && this.arousal == "low" -> "\uD83D\uDE04"
-        this.valence == "negative" && this.arousal == "mid" -> "\uD83D\uDE04"
-        this.valence == "negative" && this.arousal == "high" -> "\uD83D\uDE04"
+        this.valence == "positive" && this.arousal == "low" -> "\uD83D\uDE0C" // calm
+        this.valence == "positive" && this.arousal == "mid" -> "\uD83D\uDE42" // slight smile
+        this.valence == "positive" && this.arousal == "high" -> "☺️" // happy
+        this.valence == "negative" && this.arousal == "low" -> "\uD83D\uDE22" // sad
+        this.valence == "negative" && this.arousal == "mid" -> "\uD83D\uDE41" // slight frown
+        this.valence == "negative" && this.arousal == "high" -> "\uD83D\uDE21" // angry
         else -> "\uD83D\uDE15"
     }
 }
