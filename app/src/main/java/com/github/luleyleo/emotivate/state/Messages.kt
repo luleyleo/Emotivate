000702e156c9c0dcf25a2eaa86dcfdf768643c54
Leopold Luley <git@leopoldluley.de>
2023-03-30 11:48:51 +0200

    Add Emotion class.

--- app/src/main/java/com/github/luleyleo/emotivate/state/Messages.kt
+++ app/src/main/java/com/github/luleyleo/emotivate/state/Messages.kt
@@ -9,5 +9,19 @@ data class Message(
     val timestamp: String,
     val text: String,
     val image: Bitmap? = null,
+    val emotion: Emotion? = null,
 )
+
+@Immutable
+data class Emotion(val valence: String, val arousal: String) {
+    fun emote() = when {
+        this.valence == "positive" && this.arousal == "low" -> "\uD83D\uDE04"
+        this.valence == "positive" && this.arousal == "mid" -> "\uD83D\uDE04"
+        this.valence == "positive" && this.arousal == "high" -> "\uD83D\uDE04"
+        this.valence == "negative" && this.arousal == "low" -> "\uD83D\uDE04"
+        this.valence == "negative" && this.arousal == "mid" -> "\uD83D\uDE04"
+        this.valence == "negative" && this.arousal == "high" -> "\uD83D\uDE04"
+        else -> "\uD83D\uDE15"
+    }
+}
 
