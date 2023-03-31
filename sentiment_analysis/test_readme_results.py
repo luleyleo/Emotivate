c821ade5c341138ed1826815e4776ff34f27b0c7
Leopold Luley <git@leopoldluley.de>
2023-03-31 10:12:16 +0200

    Balance evaluation samples.

--- sentiment_analysis/test_readme_results.py
+++ sentiment_analysis/test_readme_results.py
@@ -1,6 +1,7 @@
 import seaborn as sn
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 raw_results = {
     'api': {'true_1': 796, 'false_1': 203, 'true_3': 1172, 'false_3': 549},
@@ -30,25 +31,32 @@ absolute_results = {
     }
 }
 
+total_positive = 796 + 203
+total_negative = 1172 + 549
+factor = total_positive / total_negative
+total_negative = math.floor(total_negative * factor)
+
+total_examples = total_positive + total_negative
+
 results = {
     'api': {
         'positive': {
-            True: 796 / (796 + 203),
-            False: 203 / (796 + 203),
+            True: 796,
+            False: 203,
         },
         'negative': {
-            True: 1172 / (1172 + 549),
-            False: 549 / (1172 + 549),
+            True: math.floor(1172 * factor),
+            False: math.ceil(549 * factor),
         }
     },
     'go': {
         'positive': {
-            True: 668 / (668 + 331),
-            False: 331 / (668 + 331),
+            True: 668,
+            False: 331,
         },
         'negative': {
-            True: 1351 / (1351 + 370),
-            False: 370 / (1351 + 370),
+            True: math.floor(1351 * factor),
+            False: math.ceil(370 * factor),
         }
     }
 }
@@ -70,11 +78,11 @@ def main():
 
     axis[0].set_title('API')
     api_df = pd.DataFrame(api_table, index = ['+', '-'], columns = ['+', '-'])
-    sn.heatmap(api_df, fmt='.2', annot=True, cmap='Blues', ax=axis[0])
+    sn.heatmap(api_df, fmt='', annot=True, cmap='Blues', ax=axis[0])
 
     axis[1].set_title('GoEmotions')
     go_df = pd.DataFrame(go_table, index = ['+', '-'], columns = ['+', '-'])
-    sn.heatmap(go_df, fmt='.2', annot=True, cmap='Blues', ax=axis[1])
+    sn.heatmap(go_df, fmt='', annot=True, cmap='Blues', ax=axis[1])
 
     figure.savefig('data/plots/confusion.png')
 
